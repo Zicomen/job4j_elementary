7467c00f18c44c942bf95f86206575688ce774cb
Zicomen <Zicomen@ya.ru>
2021-02-06 03:53:03 +0300

    3.3. Разбор первой программы. [#467521]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Artem Baev");
+        System.out.println("11.11.11");
+    }
+}
